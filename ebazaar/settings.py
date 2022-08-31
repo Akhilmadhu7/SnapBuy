be1070d60f62fe14042ad3188599510e165a95b6
Akhil Madhu <akhilaki028@gmail.com>
2022-08-31 20:49:13 +0530

    skjd commit

--- ebazaar/settings.py
+++ ebazaar/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR,'static')
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/Snapbuy/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
